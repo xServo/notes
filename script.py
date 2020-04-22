894e58b06427ab0b156c633add105cd5d3d7f7a3
Ryan <ryansblachowicz@gmail.com>
2020-04-21 20:01:53 -0400

    Added Tkinter GUI

--- script.py
+++ script.py
@@ -1,26 +1,44 @@
+from tkinter import *
 from cryptography.fernet import Fernet 
 import os
+
+root = Tk()
+root.configure()
 
 # config
 notes_file = "notes"
 keys_file = "keys" # CHANGE THIS TO A FLASH DRIVE A LOCATION ON A FLASHDRIVE
 
 # clears terminal
-def clear():
-    os.system("clear")
- #   print("CLEARED") # for testing
+def tk_clear():
+        for widget in root.winfo_children():
+            widget.destroy()
 
-# displays name of notes 
-def search():
-    print("Notes:")
-    for i in range(0, len(notes)):
-        print("(" + str(i + 1) + ")", notes[i])
-        
-# adds note
-def add_note():
-    name = input("Enter the name for your note. (this will be unencrypted)\n")
+def header(text, span):
+    lbl = Label(root, text=text)
+    lbl.grid(column=1, row=1, columnspan=span)
+
+
+def refresh():
+    with open(notes_file, "r") as f:
+        global notes 
+        global tokens
+        notes = []
+        tokens = []
+        line = f.readline()
+        cnt = 0
+        while line:
+            if cnt % 2 == 0:
+                notes.append(line.strip())
+            if cnt % 2 != 0:
+                tokens.append(line.strip())
+            cnt += 1
+            line = f.readline()
+
+def get_note(name_entry, entry_entry):
+    name = name_entry.get()
     name = name + "\n"
-    entry = input("Enter your note. (this will be encrypted)\n")
+    entry = entry_entry.get()
     # encrypt entry
     entry = entry.encode(encoding="UTF-8")
     key = Fernet.generate_key()
@@ -37,6 +55,23 @@ def add_note():
     with open(notes_file, "a") as f:
         f.write(name)
         f.write(token_str)
+    start()
+
+# adds note
+def add_note():
+    tk_clear()
+    # gets name
+    header("Enter a name for your note. (this will be unencrypted)", 1)
+    name_entry = Entry(root)
+    name_entry.grid(row=2, column=1)
+    # gets note
+    lbl = Label(root, text="Enter your note. (this will be encrypted)")
+    lbl.grid(column=1, row=3, columnspan=1)
+    entry_entry = Entry(root, fg="red")
+    entry_entry.grid(row=4, column=1)
+    # done button
+    btn = Button(text="Done", command=lambda: get_note(name_entry, entry_entry))
+    btn.grid(column=1, row=5)
 
 # decrypt token
 def decrypt(selection):
@@ -56,45 +91,41 @@ def decrypt(selection):
         decrypted = decrypted.decode(encoding="UTF-8")
         return decrypted
 
+# read notes
+def read_display(note):
+    global selection
+    tk_clear()
+    title = notes[int(note) - 1] + ":"
+    header(title, 1)
+    lbl = Label(root, text=decrypt(note))
+    lbl.grid(column=1, row=2, columnspan=1)
+    btn = Button(root, text="Done", command=start)
+    btn.grid(column=1, row=3, columnspan=1)
 
-clear()
-print("Welcome to Encrypted Notes")
-while True:
-    # appends each line in notes file to table
-    with open(notes_file, "r") as f:
-        notes = []
-        tokens = []
-        line = f.readline()
+def read():
+        tk_clear()
+        lbl = Label(root, text="Select a note to read.")
+        lbl.grid(column=1, row=1, columnspan=3)
+        button = []
         cnt = 0
-        while line:
-            if cnt % 2 == 0:
-                notes.append(line.strip())
-            if cnt % 2 != 0:
-                tokens.append(line.strip())
-            cnt += 1
-            line = f.readline()
+        for i in notes:
+            cnt +=1
+            button.append(Button(root, text=i, command=lambda cnt=cnt: read_display(cnt)))
+            button[cnt - 1].grid(column=cnt, row=2)
 
-    # Select action
-    action = input("What would you like to do?\n(1) Read Notes\n(2) Take Notes\n(3) Exit\n")
+def start():
+    tk_clear()
+    # begin
+    refresh()
+    # main display
+    display = "Please select an option."
+    lbl = Label(root, text=display)
+    lbl.grid(column=1, row=1, columnspan=2)
+    # buttons
+    read_button = Button(root, text="Read Notes", command=read)
+    read_button.grid(column=1, row=2)
+    add_button = Button(root, text="Add Notes", command=add_note)
+    add_button.grid(column=2, row=2)
 
-    # Read Notes
-    if action == "1":
-        clear()
-        search()
-        selection = input("Select a note\n")
-        clear()
-        print(notes[int(selection) - 1] + ":")
-        print(decrypt(selection))
-#        print(tokens[int(selection) - 1])
-        input("Press ENTER to continue.")
-        clear()
-
-    # Take notes
-    if action == "2":
-        clear()
-        add_note()
-        clear()
-
-    if action == "3":
-        clear()
-        break
+start()
+root.mainloop()
